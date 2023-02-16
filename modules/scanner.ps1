7a489ee8adc00ab1d4a36e315d80de736835f81c
b3rg <bergb9443@gmail.com>
2023-02-16 10:49:32 -0500

    refactoring scanner.ps1 to add a menu to the module

--- modules/scanner.ps1
+++ modules/scanner.ps1
@@ -1,28 +1,53 @@
-function getVersionTable() {
-    $PSVersionTable | ConvertTo-Html | Out-File versionTable.html
+function title() {
+    Write-Host "=========================================================="
+    Write-Host 
+    "
+     _ __ ___   ___   __| |_   _| | __ _ _ __   _ __ __ _| |_
+    | '_ ` _ \ / _ \ / _` | | | | |/ _` | '__| | '__/ _` | __|
+    | | | | | | (_) | (_| | |_| | | (_| | |    | | | (_| | |_
+    |_| |_| |_|\___/ \__,_|\__,_|_|\__,_|_|    |_|  \__,_|\__|
+    "
+    Write-Host "Module : Scanner"
+    Write-Host "==========================================================="
+    Write-Host ""
 }
 
-function getExecutionPolicy() {
-    Get-ExecutionPolicy -List | ConvertTo-Html | Out-File executionPolicy.html
-}
-function getProcessRunning() {
-    Get-Process | ConvertTo-Html | Out-File reportProcess.html
-}
-
-function getNetIPAddress() {
-    Get-NetIPAddress | ConvertTo-Html | Out-File reportNetIp.html
-}
-
-function getNetIPAddress() {
-    Get-HoxFix | ConvertTo-Html | Out-File reportHotFixes.html
+function menu() {
+    Write-Host "Press v and enter to show ps version table"
+    Write-Host "Press e and enter to show execution list policy"
+    Write-Host "Press p and enter to show process running"
+    Write-Host "Press n and enter to show net ip address information"
+    Write-Host "Press f and enter to show hot fixes"
+    Write-Host "Press s and enter to show services"
+    Write-Host "Press w and enter to show windows defender details"
+    Write-Host "Press q and enter to quit"
 }
 
-function getServices() {
-    Get-Service | ConvertTo-Html | Out-File reportServices.html
-}
-function checkIfWindowsDefenderIsRunning() {
-    Get-Service Windefend, SecurityHealthService, wscsvc | Select-Object Name, DisplayName, Status | ConvertTo-Html | Out-File reportWinDefender.html
+function ouputFile($data) {
+    $answer = Read-Host "Do you want to output a file as html?(y/n)"
+                
+    if ($answer -eq "y") {
+        $fileName = Read-Host "Specify the name of the file without the extension"
+        $data | ConvertTo-Html | Out-File "$fileName.html"
+    }
+    Write-Host "$data" 
 }
 
-getExecutionPolicy
-checkIfWindowsDefenderIsRunning
+function run() {
+    do {
+        title
+        menu
+
+        $option = Read-Host "Choose an option"
+
+        switch ($option) {
+            "v" { ouputFile($PSVersionTable) }
+            "e" { ouputFile(Get-ExecutionPolicy -List) }
+            "p" { ouputFile(Get-Process) }
+            "n" { ouputFile(Get-NetIPAddress) }
+            "f" { ouputFile(Get-HoxFix) }
+            "s" { ouputFile(Get-Service) }
+            "w" { ouputFile(Get-Service Windefend, SecurityHealthService, wscsvc | Select-Object Name, DisplayName, Status) }
+        }
+    }while ($option -ne "q") 
+}
